84870dc4ccb745f4e8835ec0cb7e47b042bc765f
Andre Murphy <42878649+FixedSpecialty@users.noreply.github.com>
2021-04-01 01:28:42 -0600

    Update script.js

--- src/script.js
+++ src/script.js
@@ -14,6 +14,10 @@ const delay = 6000
     {
        // console.log("loading started")
         document.getElementById('wrapper').style.display = "block"
+     setTimeout(() =>
+        {
+            delay
+        })
     }
     manager.onProgress = () =>
     {
